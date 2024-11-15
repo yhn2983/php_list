e9a4af3c3f460a433b48c931563c3a792936dd8c
yang <yhn2983@gmail.com>
2024-11-15 21:55:13 +0800

    navbar修改

--- parts/navbar.php
+++ parts/navbar.php
@@ -5,17 +5,17 @@ if (!isset($pageName)) {
 ?>
 
 <style>
-  .navbar-nav .nav-link {
-    padding-left: 5px;
-  }
+.navbar-nav .nav-link {
+  padding-left: 5px;
+}
 
-  .navbar-nav .nav-link.active {
-    background-color: lightskyblue;
-    color: darkblue;
-    border-radius: 5px;
-    font-weight: 800;
-    padding-left: 5px;
-  }
+.navbar-nav .nav-link.active {
+  background-color: lightskyblue;
+  color: darkblue;
+  border-radius: 5px;
+  font-weight: 800;
+  padding-left: 5px;
+}
 </style>
 
 <div class="px-0 mx-0 shadow">
@@ -28,35 +28,35 @@ if (!isset($pageName)) {
       </button>
       <div class="collapse navbar-collapse" id="navbarSupportedContent">
         <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-
+          <?php if ($pageName !== "member"):  ?>
           <li class="nav-item">
             <a class="nav-link <?= $pageName == 'list' ? 'active' : '' ?>" href="list.php">列表</a>
           </li>
-          <?php if ($pageName !== "member"):  ?>
-            <li class="nav-item">
-              <a class="nav-link <?= $pageName == 'add' ? 'active' : '' ?>" href="add.php">新增</a>
-            </li>
-          <?php endif; ?>
+
+          <li class="nav-item">
+            <a class="nav-link <?= $pageName == 'add' ? 'active' : '' ?>" href="add.php">新增</a>
+          </li>
+
           <li class="nav-item">
             <a class="nav-link <?= $pageName == 'eval_list' ? 'active' : '' ?>" href="eval_list.php">評論</a>
-          </li>
+          </li> <?php endif; ?>
         </ul>
 
         <ul class="navbar-nav mb-2 mb-lg-0">
           <?php if (isset($_SESSION['admin'])) : ?>
-            <li class="nav-item">
-              <a class="nav-link "><?= $_SESSION['admin']['nickname'] ?></a>
-            </li>
-            <li class="nav-item">
-              <a class="nav-link" href="logout.php">登出</a>
-            </li>
+          <li class="nav-item">
+            <a class="nav-link "><?= $_SESSION['admin']['nickname'] ?></a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="logout.php">登出</a>
+          </li>
           <?php else : ?>
-            <li class="nav-item">
-              <a class="nav-link <?= $pageName == 'login' ? 'active' : '' ?>" href="login.php">登入</a>
-            </li>
-            <li class="nav-item">
-              <a class="nav-link <?= $pageName == 'regist' ? 'active' : '' ?>" href="register.php">註冊</a>
-            </li>
+          <li class="nav-item">
+            <a class="nav-link <?= $pageName == 'login' ? 'active' : '' ?>" href="login.php">登入</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link <?= $pageName == 'regist' ? 'active' : '' ?>" href="register.php">註冊</a>
+          </li>
           <?php endif; ?>
         </ul>
       </div>
