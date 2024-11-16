1b1d8531d024c7c876e002d3195ab056f4711265
yang <yhn2983@gmail.com>
2024-11-16 22:40:30 +0800

    修正db_link

--- parts/db_link.php
+++ parts/db_link.php
@@ -1,21 +1,20 @@
 <?php
-$db_host="localhost";
-$db_user="root";
-$db_pass="E124801602";
-$db_name="fake_data_base";
+$db_host = "localhost";
+$db_user = "root";
+$db_pass = "password";
+$db_name = "database_name";
 
-$dsn="mysql:host={$db_host};dbname={$db_name};charset=utf8mb4";
+$dsn = "mysql:host={$db_host};dbname={$db_name};charset=utf8mb4";
 
-$pdo_option=[
-    PDO::ATTR_DEFAULT_FETCH_MODE=>PDO::FETCH_ASSOC,
-    PDO::ATTR_ERRMODE=>
+$pdo_option = [
+    PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
+    PDO::ATTR_ERRMODE =>
     PDO::ERRMODE_EXCEPTION
 ];
 
-$pdo=new PDO($dsn,$db_user,$db_pass,$pdo_option);
+$pdo = new PDO($dsn, $db_user, $db_pass, $pdo_option);
 
 
-if(!isset($_SESSION)){
+if (!isset($_SESSION)) {
     session_start();
 }
-
